aa1a63f2ef054cb8b770df0c52dbe5aae94b382f
fkdkent <fkdkent@agileware.jp>
2020-09-21 15:11:46 +0900

    Clarify variable name

--- test/unit/issue_test.rb
+++ test/unit/issue_test.rb
@@ -2923,12 +2923,12 @@ class IssueTest < ActiveSupport::TestCase
 
   # done_ratio should change to 100 when the status is closed and Setting.issue_done_ratio equal issue_field_and_closed_status
   test "when updating to closed status should update done_ratio according to Setting.issue_done_ratio" do
-    issue_status = IssueStatus.find(5)
-    issue_status.update!(:default_done_ratio => 90)
+    closed_status = IssueStatus.find(5)
+    closed_status.update!(:default_done_ratio => 90)
 
     with_settings :issue_done_ratio => 'issue_field' do
       issue = Issue.generate!(:status_id => 1, :done_ratio => 30)
-      issue.status = issue_status
+      issue.status = closed_status
       issue.save!
 
       assert_equal 30, issue.read_attribute(:done_ratio)
@@ -2936,7 +2936,7 @@ class IssueTest < ActiveSupport::TestCase
 
     with_settings :issue_done_ratio => 'issue_status' do
       issue = Issue.generate!(:status_id => 1, :done_ratio => 30)
-      issue.status = issue_status
+      issue.status = closed_status
       issue.save!
 
       assert_equal 90, issue.read_attribute(:done_ratio)
@@ -2944,7 +2944,7 @@ class IssueTest < ActiveSupport::TestCase
 
     with_settings :issue_done_ratio => 'issue_field_and_closed_status' do
       issue = Issue.generate!(:status_id => 1, :done_ratio => 30)
-      issue.status = issue_status
+      issue.status = closed_status
       issue.save!
 
       assert_equal 100, issue.read_attribute(:done_ratio)
