43e4f273c2cac5adeb5c7ae28e5d5b3e179e39e0
Kevin Fischer <kevin@agileware.jp>
2021-02-15 12:09:48 +0900

    Add WikiRedirectsController with destroy action

---
+++ app/controllers/wiki_redirects_controller.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+# Redmine - project management software
+# Copyright (C) 2006-2020  Jean-Philippe Lang
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+class WikiRedirectsController < ApplicationController
+  before_action :find_wiki_redirect, :authorize
+
+  def destroy
+    @wiki_redirect.destroy
+  end
+
+  private
+
+  def find_wiki_redirect
+    @project = Project.find(params[:project_id])
+    page = Wiki.find_page(params[:wiki_page_id], project: @project)
+    @wiki_redirect= WikiRedirect.where(redirects_to: page.title).find(params[:id])
+    render_404 unless @wiki_redirect
+  rescue ActiveRecord::RecordNotFound
+    render_404
+  end
+end
