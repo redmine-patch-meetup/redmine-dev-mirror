ced18efbbbf9b28f0f119ae4a22a9f93ea1760a7
Akihiro MATOBA <matobaa+github@gmail.com>
2021-03-23 09:30:09 +0900

    Fix a test bug; add loading user_preferences fixture

--- test/integration/custom_field_timewithzone_test.rb
+++ test/integration/custom_field_timewithzone_test.rb
@@ -22,6 +22,7 @@ require File.expand_path('../../test_helper', __FILE__)
 class CustomFieldsTimewithzoneTest < Redmine::IntegrationTest
   fixtures :projects,
            :users, :email_addresses,
+           :user_preferences,
            :roles,
            :members,
            :member_roles,
