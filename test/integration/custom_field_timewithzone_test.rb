098f0ceb8e1f06ceef9b7be4c5e6a19beacb9573
Akihiro MATOBA <matobaa+github@gmail.com>
2022-01-15 13:11:41 +0000

    split test method

--- test/integration/custom_field_timewithzone_test.rb
+++ test/integration/custom_field_timewithzone_test.rb
@@ -28,65 +28,75 @@ class CustomFieldsTimewithzoneTest < Redmine::IntegrationTest
            :member_roles,
            :trackers,
            :projects_trackers,
-           :enabled_modules,
-           :issue_statuses,
            :issues,
            :enumerations,
            :custom_fields,
            :custom_values,
-           :custom_fields_trackers,
-           :attachments
+           :custom_fields_trackers
 
-  def test_issue_with_timewithzone_custom_field
-    field = IssueCustomField.find(12)
-    # TODO default_language, default_timezone
+  def setup
+    @field = IssueCustomField.find(12)
+    @issue = Issue.find(14)
     log_user('jsmith', 'jsmith')
-    user = User.find(session[:user_id])
-    
+    @user = User.find(session[:user_id])
+  end
+
+  def test_get_issue_with_timewithzone_custom_field
+    assert_nil ENV['TZ']
+    assert_equal 'UTC', RedmineApp::Application.config.time_zone
+    assert_equal :local, config.default_timezone
+    assert_equal 'en', Setting.default_language
+
     # get issues/14 in tz=nil
-    assert_nil user.preference.time_zone
-    get '/issues/14'
+    assert_nil @user.preference.time_zone
+    get "/issues/#{@issue.id}"
     assert_response :success
-    assert_select ".cf_#{field.id} .value", :text => '03/11/2011 05:46 AM'
-    assert_select 'input[name=?][value=?]', "issue[custom_field_values][#{field.id}]", '2011-03-11T05:46'
+    assert_select ".cf_#{@field.id} .value", :text => '03/11/2011 05:46 AM'
+    assert_select 'input[name=?][value=?]', "issue[custom_field_values][#{@field.id}]", '2011-03-11T05:46'
     
     # get issues/14 in tz='UTC'
-    user.preference.time_zone = 'UTC'
-    user.preference.save
-    get '/issues/14'
+    @user.preference.time_zone = 'UTC'
+    @user.preference.save
+    get "/issues/#{@issue.id}"
     assert_response :success
-    assert_select ".cf_#{field.id} .value", :text => '03/11/2011 05:46 AM'
-    assert_select 'input[name=?][value=?]', "issue[custom_field_values][#{field.id}]", '2011-03-11T05:46'
+    assert_select ".cf_#{@field.id} .value", :text => '03/11/2011 05:46 AM'
+    assert_select 'input[name=?][value=?]', "issue[custom_field_values][#{@field.id}]", '2011-03-11T05:46'
     
     # get issues/14 in lang="ja", tz='Tokyo' (+0900)
-    user.preference.time_zone = 'Tokyo'
-    user.preference.save
-    user.language = "ja"
-    user.save
-    get '/issues/14'
+    @user.preference.time_zone = 'Tokyo'
+    @user.preference.save
+    @user.language = "ja"
+    @user.save
+    get "/issues/#{@issue.id}"
     assert_response :success
-    assert_select ".cf_#{field.id} .value", :text => '2011/03/11 14:46'
-    assert_select 'input[name=?][value=?]', "issue[custom_field_values][#{field.id}]", '2011-03-11T14:46'
+    assert_select ".cf_#{@field.id} .value", :text => '2011/03/11 14:46'
+    assert_select 'input[name=?][value=?]', "issue[custom_field_values][#{@field.id}]", '2011-03-11T14:46'
+  end
 
+  def test_put_issue_with_timewithzone_custom_field
     # update issues/14 in lang="ja", tz='Tokyo' (+0900)
-    put '/issues/14',
+    @user.preference.time_zone = 'Tokyo'
+    @user.preference.save
+    @user.language = "ja"
+    @user.save
+    put "/issues/#{@issue.id}",
     :params => {
       :issue => {
-        :custom_field_values => {field.id.to_s => "1985-08-12T18:56"}  # in tz=Asia/Tokyo +0900
+        :custom_field_values => {@field.id.to_s => "1985-08-12T18:56"}  # in tz=Asia/Tokyo +0900
       }
     }
-    assert_equal "1985-08-12T09:56:00Z", CustomValue.find_by(:customized_id=>14, :custom_field_id => 12).value
-
+    assert_equal "1985-08-12T09:56:00Z", CustomValue.find_by(:customized_id=>@issue.id, :custom_field_id => @field.id).value
+    
     # update issues/14 in tz=nil
-    user.preference.time_zone = nil
-    user.preference.save
-    put '/issues/14',
+    @user.preference.time_zone = nil
+    @user.preference.save
+    put "/issues/#{@issue.id}",
     :params => {
       :issue => {
-        :custom_field_values => {field.id.to_s => "1912-04-14T23:40"}  # in UTC
+        :custom_field_values => {@field.id.to_s => "1912-04-14T23:40"}  # in UTC
       }
     }
-    assert_equal "1912-04-14T23:40:00Z", CustomValue.find_by(:customized_id=>14, :custom_field_id => 12).value
+    assert_equal "1912-04-14T23:40:00Z", CustomValue.find_by(:customized_id=>@issue.id, :custom_field_id => @field.id).value
   end
 
 end
